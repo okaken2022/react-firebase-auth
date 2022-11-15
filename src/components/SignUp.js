a7c9385cecbb6fecde271ca1d6d46ff177277b7e
okaken2022 <okaken.address@gmail.com>
2022-11-15 20:31:04 +0900

    initial commit & inputの取得

---
+++ src/components/SignUp.js
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(email, password);
+  };
+  const handleChangeEmail = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+  const handleChangePassword = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+
+  return (
+    <div>
+      <h1>ユーザ登録</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>メールアドレス</label>
+          <input
+            name="email"
+            type="email"
+            placeholder="email"
+            onChange={(event) => handleChangeEmail(event)}
+          />
+        </div>
+        <div>
+          <label>パスワード</label>
+          <input
+            name="password"
+            type="password"
+            placeholder="password"
+            onChange={(event) => handleChangePassword(event)}
+          />
+        </div>
+        <div>
+          <button>登録</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
